a578d7fae393a415615ece102e250e9de7f5b742
İbrahim ALPAK <aribilgiogretmen@gmail.com>
2024-07-07 10:54:43 +0300

    Add project files.

---
+++ CVAPP/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CVAPP.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
